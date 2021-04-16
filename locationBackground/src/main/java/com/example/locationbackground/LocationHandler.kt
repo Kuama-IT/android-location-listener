1f7f8e38c2233bb1eb5e31768d20b2b296048e0b
matteoKuama <matteo@kuama.net>
2021-04-16 15:28:50 +0200

    Added builder for LocationHandler.kt Removed all the dependencies from the specific activity in the constructor

--- locationBackground/src/main/java/com/example/locationbackground/LocationHandler.kt
+++ locationBackground/src/main/java/com/example/locationbackground/LocationHandler.kt
@@ -1,6 +1,8 @@
 package com.example.locationbackground
 
-import android.app.Activity
+import android.Manifest.*
+import android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
@@ -8,18 +10,131 @@ import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationManager
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import java.lang.Exception
 import java.util.*
 
-class LocationHandler(private val myActivity: Activity) {
+interface Broadcaster {
+    fun broadcast(intent: Intent)
+}
 
-    //request id, selected random num
-    private val requestId = 80
-    private var fusedLocationClient = LocationServices.getFusedLocationProviderClient(myActivity)
-    private val actionName = "com.example.backgroundlocation"
+interface LocationGetter {
+    fun sendRequest()
+}
+
+interface GeocodeBuilder {
+    fun getGeocoder():Geocoder
+}
+
+class LocationHandler private constructor(
+    private val fusedLocationClient: FusedLocationProviderClient,
+    private val locationManager: LocationManager,
+    private val broadcaster: Broadcaster,
+    private val locationGetter: LocationGetter,
+    private val geocodeBuilder: GeocodeBuilder,
+    val onError: ((Throwable) -> Unit)?
+) {
+
+    companion object {
+        const val REQUEST_ID = 1000
+
+        private fun checkPermission(context: Context, permission: String) =
+            ActivityCompat.checkSelfPermission(
+                context,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+
+        private fun checkPermissions(context: Context): Boolean =
+            checkPermission(context, ACCESS_BACKGROUND_LOCATION)
+                    || checkPermission(context, permission.ACCESS_FINE_LOCATION)
+
+        /**
+         * Check if the GPS services are active
+         */
+        private fun checkGPSActive(locationManager: LocationManager): Boolean {
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        }
+    }
+
+    class Builder {
+
+        private var context: Context? = null
+        fun context(context: Context) = apply {
+            this.context = context
+        }
+
+
+        private var onError: ((Throwable) -> Unit)? = null
+        fun onError(onError: (Throwable) -> Unit) = apply {
+            this.onError = onError
+        }
+
+        /**
+         * @throws IllegalStateException when location permissions are not granted
+         * @throws IllegalStateException when GPS is off
+         */
+        fun build(): LocationHandler {
+            context?.let {
+                if (!checkPermissions(it)) {
+                    error("Please require user's permission to use either ACCESS_BACKGROUND_LOCATION or ACCESS_FINE_LOCATION before using this class")
+                }
+
+                val locationManager =
+                    it.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+                if (!checkGPSActive(locationManager)) {
+                    error("Please activate GPS before using this class")
+                }
+
+                return LocationHandler(
+                    LocationServices.getFusedLocationProviderClient(it),
+                    locationManager = locationManager,
+                    broadcaster = object : Broadcaster {
+                        override fun broadcast(intent: Intent) {
+                            it.sendBroadcast(intent)
+                        }
+                    },
+                    locationGetter = object : LocationGetter {
+                        val locationRequest = LocationRequest.create()
+                            .apply {
+                                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                                interval = 0
+                                fastestInterval = 0
+                            }
+
+                        @SuppressLint("MissingPermission")
+                        override fun sendRequest() {
+                            val intent = Intent(it, it::class.java)
+                            LocationServices.getFusedLocationProviderClient(it)
+                                .requestLocationUpdates(
+                                    locationRequest,
+                                    PendingIntent.getService(
+                                        it,
+                                        REQUEST_ID,
+                                        intent,
+                                        PendingIntent.FLAG_UPDATE_CURRENT
+                                    )
+                                )
+                        }
+                    },
+                    geocodeBuilder = object : GeocodeBuilder{
+                        override fun getGeocoder() :Geocoder {
+                            return Geocoder(it, Locale.getDefault())
+                        }
+                    },
+                    onError = onError
+                )
+            } ?: error("Please provide a context using the context() method before calling build()")
+
+        }
+    }
+
+
+    private val actionName = javaClass.name
     private lateinit var locationRequest: LocationRequest
 
     //each time we set a new value to the location, calls sendIntent that sends a broadcast
@@ -32,44 +147,6 @@ class LocationHandler(private val myActivity: Activity) {
 
     var accuracy: Float? = null
 
-
-    /**
-     * Check if we have the permission to access the position
-     */
-    fun checkPermission(): Boolean {
-
-        return (ActivityCompat.checkSelfPermission(
-            myActivity, android.Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(
-            myActivity, android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-                )
-    }
-
-
-    /**
-     * Get the user permissions to use the position
-     */
-    private fun getPermission() {
-        ActivityCompat.requestPermissions(
-            myActivity, arrayOf(
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ), requestId
-        )
-    }
-
-    /**
-     * Check if the GPS services are active
-     */
-    private fun checkGPSActive(): Boolean {
-        val locationManager =
-            myActivity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-    }
 
     /**
      * Retrieve the last known location
@@ -91,79 +168,58 @@ class LocationHandler(private val myActivity: Activity) {
             putExtra("city", getCity())
             putExtra("country", getCountry())
         }
-        myActivity.sendBroadcast(intent)
+
+        broadcaster.broadcast(intent)
     }
 
 
     /**
      * Return the last known location
+     *
+     * No need to check for permissions, see [Builder.build]
      */
+    @SuppressLint("MissingPermission")
     private fun getLastKnownLocation(): Location? {
-        if (checkPermission()) {
-            if (checkGPSActive()) {
-                fusedLocationClient.lastLocation.addOnSuccessListener { task: Location?
-                    ->
-                    //if task is not null assign the new value of the location
-                    if (task != null) {
-                        location = task
-                        //set accuracy
-                        if (accuracy != null)
-                            task.accuracy = accuracy as Float
-                    } else {
-                        //retrieve new value of the location
-                        getLocation()
-                    }
-
-                }
-
-                fusedLocationClient.lastLocation.addOnFailureListener { task: Exception ->
-                    getLocation()
-                    //toast for errors
-                    Toast.makeText(myActivity, task.message, Toast.LENGTH_SHORT).show()
-                }
+        fusedLocationClient.lastLocation.addOnSuccessListener { task: Location?
+            ->
+            //if task is not null assign the new value of the location
+            if (task != null) {
+                location = task
+                //set accuracy
+                if (accuracy != null)
+                    task.accuracy = accuracy as Float
             } else {
-                Toast.makeText(
-                    myActivity, "Please activate your GPS", Toast.LENGTH_SHORT
-                ).show()
+                //retrieve new value of the location
+                getLocation()
             }
-        } else {
-            getPermission()
+
         }
+
+        fusedLocationClient.lastLocation.addOnFailureListener { error: Exception ->
+            getLocation()
+            //toast for errors
+            onError?.invoke(error)
+        }
+
         return location
     }
 
 
     /**
-     * Set and send the request to get the location
+     * Send the request to get the location
      */
     private fun getLocation() {
-        locationRequest = LocationRequest.create()
-        locationRequest.apply {
-            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            interval = 0
-            fastestInterval = 0
-        }
-        if (checkPermission())
-            myActivity.intent = Intent(myActivity, myActivity::class.java)
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            PendingIntent.getService(
-                myActivity,
-                requestId,
-                myActivity.intent,
-                PendingIntent.FLAG_UPDATE_CURRENT
-            )
-        )
+        locationGetter.sendRequest()
     }
 
     /**
      * Return the current city
      */
-    fun getCity(): String? {
+    private fun getCity(): String? {
         var city = ""
-        val geocoder = Geocoder(myActivity, Locale.getDefault())
         location?.let {
-            val result = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+            val result = geocodeBuilder.getGeocoder()
+                .getFromLocation(it.latitude, it.longitude, 1)
             city = result[0].locality
             return city
         }
@@ -173,11 +229,11 @@ class LocationHandler(private val myActivity: Activity) {
     /**
      * Return the current country name
      */
-    fun getCountry(): String? {
+    private fun getCountry(): String? {
         var country = ""
-        val geocoder = Geocoder(myActivity, Locale.getDefault())
         location?.let {
-            val result = geocoder.getFromLocation(it.latitude, it.longitude, 1)
+            val result = geocodeBuilder.getGeocoder()
+                .getFromLocation(it.latitude, it.longitude, 1)
             country = result[0].countryName
             return country
         }
