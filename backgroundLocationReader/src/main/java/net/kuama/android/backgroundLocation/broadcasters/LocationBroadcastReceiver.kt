d037f673cc9645c8f91e6540597bc584d3580686
matteoKuama <matteo@kuama.net>
2021-04-26 12:11:11 +0200

    LocationBroadcastReceiver.kt is abstract. To use the functionality of this library you need to extend this class and implement the abstract method onLocation()

--- backgroundLocationReader/src/main/java/net/kuama/android/backgroundLocation/broadcasters/LocationBroadcastReceiver.kt
+++ backgroundLocationReader/src/main/java/net/kuama/android/backgroundLocation/broadcasters/LocationBroadcastReceiver.kt
@@ -9,22 +9,16 @@ import android.util.Log
  * Custom broadcast receiver that reads the Intent sent from [net.kuama.android.backgroundLocation.LocationRequestManager]
  * The Intent contains two extra fields: latitude and longitude
  * Modify the action in this method for your custom use
- *
- * TODO ADD ABSTRACT CLASS AND REMOVE THE COMMENTS
  */
-class LocationBroadcastReceiver : BroadcastReceiver() {
+abstract class LocationBroadcastReceiver : BroadcastReceiver() {
 
     /**
      * Once a broadcast is sent by [net.kuama.android.backgroundLocation.LocationRequestManager]
      * it reads the content of the intent, in particular the extras containing the latitude and longitude
      */
     override fun onReceive(context: Context?, intent: Intent?) {
-        // intent?.extras?.let { onLocation(it.getDouble("latitude"), it.getDouble("longitude")) }
-        Log.d(
-            "Location",
-            "\nlatitude:${intent!!.extras!!["latitude"]}\nlongitude${intent.extras!!["longitude"]}"
-        )
+        intent?.extras?.let { onLocation(it.getDouble("latitude"), it.getDouble("longitude")) }
     }
 
-    //  abstract fun onLocation(latitude:Double, longitude:Double)
+    abstract fun onLocation(latitude:Double, longitude:Double)
 }
